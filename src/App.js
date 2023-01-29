393dcf76ba62133bc4e4633c7e5498346e46eea1
Hamdia Abdulai <hamdiaabdulai85@gmail.com>
2023-01-29 00:01:46 +0000

    firstpage

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import './App.css';
+import Navbar from './Components/Navbar'
+import './index.css'
+
+import Firstpage from './Components/Firstpage'
+
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+      <Firstpage/>
       
     </div>
   );
